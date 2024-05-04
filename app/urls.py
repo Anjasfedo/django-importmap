e0251c398a311310ba8a15815c326a32998a520c
AnjasFedo <fedoaf98@gmail.com>
2024-05-04 10:52:11 +0700

    Success get Response From Server

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('qr_codes', views.qr_code_create, name='qr_code_create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -6,3 +7,13 @@ def index(request):
     context = {}
     
     return render(request, 'index.html', context)
+
+def qr_code_create(request):
+    if request.method == 'POST':
+        qr_data = request.POST.get('qr_data')
+        # Lakukan sesuatu dengan qr_data, seperti menyimpannya ke database
+        # Misalnya:
+        # QRCode.objects.create(data=qr_data)
+        return JsonResponse({'message': 'QR code data received successfully', 'data': qr_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Only POST requests are allowed'}, status=400)
